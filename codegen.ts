504e452ae3fc8bff4e74f52ca3039cd1fa01bbb9
Nathan <chicanathan@gmail.com>
2025-07-09 06:57:04 -0700

    added graphql codegen and adjusted linter configs

---
+++ codegen.ts
@@ -0,0 +1,25 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+    overwrite: true,
+    schema: './src/schema/**/*.graphql',
+    generates: {
+        'src/generated/graphql.ts': {
+            plugins: ['typescript', 'typescript-resolvers'],
+            config: {
+                useIndexSignature: true,
+                contextType: '../context#GraphQLContext',
+                mappers: {
+                    User: '@prisma/client#User as PrismaUser',
+                    LoggableEvent: '@prisma/client#LoggableEvent as PrismaLoggableEvent',
+                    EventLabel: '@prisma/client#EventLabel as PrismaEventLabel'
+                },
+                scalars: {
+                    DateTime: 'Date'
+                }
+            }
+        }
+    }
+};
+
+export default config;
